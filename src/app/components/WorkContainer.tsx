b418990cd4e7a53402420bf0cb4ba9af51ae6153
Aron Hawkins <aron@sidekick.agency>
2023-08-25 00:02:53 -0500

    Moved apps and websites into components

--- src/app/components/WorkContainer.tsx
+++ src/app/components/WorkContainer.tsx
@@ -1,10 +1,14 @@
-import { ProjectCard } from './ProjectCard';
+import { AppCard } from './AppCard';
 import { BsPhone } from 'react-icons/bs';
 
 import { MessageAppCard } from './MessageAppCard';
 import { AirbnbAppCard } from './AirbnbAppCard';
 import { RestaurantAppCard } from './RestaurantAppCard';
 import { ThreeDCard } from './ThreeDCard';
+import { AppType, WebsiteType } from '../types/types';
+import { PiHouse } from 'react-icons/pi';
+import { IoFastFoodOutline } from 'react-icons/io5';
+import { WebsiteCard } from './WebsiteCard';
 
 export const WorkContainer = () => {
 	return (
@@ -21,10 +25,33 @@ export const WorkContainer = () => {
 					<div className='w-[1px] h-16 bg-white/50'></div>
 				</div>
 				<div className='flex flex-col gap-4 w-full lg:max-w-6xl'>
-					<MessageAppCard />
-					<AirbnbAppCard />
-					<RestaurantAppCard />
-					<ThreeDCard />
+					<AppCard
+						Icon={BsPhone}
+						title='Messenger App'
+						description='Send and receive messages to other users, either individually or in group chats.'
+						type={AppType.Message}
+						link='https://messenger-clone-ebon.vercel.app'
+						mediaLinks={['chat_screen', 'chat_info', 'edit_info', 'group_chat']}
+						techStack={['Next.js', 'NextAuth', 'TypeScript', 'TailwindCSS', 'Pusher.js', 'Prisma', 'MongoDB']}
+					/>
+					<AppCard
+						Icon={PiHouse}
+						title='Airbnb Clone'
+						description='Working clone of the popular web app'
+						type={AppType.Airbnb}
+						link='https://next13-airbnb-clone-psi.vercel.app'
+						mediaLinks={['home_screen', 'login', 'dates', 'property']}
+						techStack={['Next.js', 'NextAuth', 'TypeScript', 'TailwindCSS', 'Prisma', 'MongoDB']}
+					/>
+					<AppCard
+						Icon={IoFastFoodOutline}
+						title='Restaurant Voting App'
+						description='Vote with friends about where to go to eat'
+						type={AppType.Vote}
+						link='https://sidekick-decide-app.vercel.app/'
+						mediaLinks={['choose_location', 'location_default', 'vote', 'change_vote']}
+						techStack={['Next.js', 'Clerk', 'TypeScript', 'TailwindCSS', 'Pusher.js', 'Google Maps API', 'Prisma', 'PostgreSQL', 'Supabase']}
+					/>
 				</div>
 			</div>
 			<div className='flex gap-16 justify-between items-center w-full'>
@@ -40,14 +67,24 @@ export const WorkContainer = () => {
 				</div>
 
 				<div className='flex flex-col gap-4 w-full lg:max-w-6xl'>
-					<MessageAppCard />
-					<AirbnbAppCard />
-					<RestaurantAppCard />
-					<ThreeDCard />
-					<MessageAppCard />
-					<AirbnbAppCard />
-					<RestaurantAppCard />
-					<ThreeDCard />
+					<WebsiteCard
+						type={WebsiteType.ecommerce}
+						title='Sendero Provisions Co.'
+						description="Built a custom theme for a multi-million dollar clothing brand's Shopify store"
+						link='https://senderopc.com'
+					/>
+					<WebsiteCard
+						type={WebsiteType.ecommerce}
+						title="Dr. Gregory's Canine Joint Formula"
+						description="Built a custom theme for dog supplement brand's Shopify store"
+						link='https://caninejointsupplement.com'
+					/>
+					<WebsiteCard
+						type={WebsiteType.business}
+						title='Old Bethany Venue'
+						description='Business website for a popular wedding and event venue in central Texas'
+						link='https://oldbethanyvenue.com'
+					/>
 				</div>
 			</div>
 		</div>
---
+++ src/app/types/types.ts
@@ -0,0 +1,13 @@
+export enum AppType {
+	Message = 1,
+	Airbnb,
+	Vote,
+	_3D,
+}
+
+export enum WebsiteType {
+	ecommerce = 1,
+	business,
+	blog,
+	nonprofit,
+}
