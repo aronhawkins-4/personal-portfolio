05fc821a545137cfa2438f3b38548d7f6a394efb
Aron Hawkins <aron@sidekick.agency>
2023-08-28 13:19:38 -0500

    style edits

--- src/app/components/ContentCard.tsx
+++ src/app/components/ContentCard.tsx
@@ -8,10 +8,10 @@ export const ContentCard = () => {
 	const [buttonToggle, setButtonToggle] = useState(false);
 	return (
 		<div className='flex flex-col gap-4 w-full items-center'>
-			<div className='p-2 w-96 relative flex justify-between items-center rounded-2xl overflow-hidden  shadow-sm shadow-slate-300/20 z-10 bg-gradient-to-tr from-transparent to-gray-400/50'>
+			<div className='p-2 w-full md:w-96 relative flex justify-between items-center rounded-2xl overflow-hidden shadow-sm shadow-slate-300/20 z-10 bg-gradient-to-tr from-transparent to-gray-400/50'>
 				<div
-					className={`w-[50%] absolute top-1/2 ${!buttonToggle && 'left-1'} ${
-						buttonToggle && 'left-[11.75rem]'
+					className={`top-1/2 left-1 w-[50%] absolute  ${!buttonToggle && 'left-1'} ${
+						buttonToggle && 'left-[49%]'
 					} h-5/6 -translate-y-1/2 bg-white rounded-xl mix-blend-difference pointer-events-none transition-all`}
 				></div>
 				<div className='w-full'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Footer } from './components/Footer';
 export default function Home() {
 	return (
 		<main className='min-h-screen overflow-hidden'>
-			<div className='flex flex-col items-center justify-start p-16 relative gap-8'>
+			<div className='flex flex-col items-center justify-start p-8 md:p-16 relative gap-8'>
 				<div className='absolute top-[-200px] right-[-10%] bg-gradient-radial from-pink-600 to-transparent w-96 h-96 rounded-full blur-2xl -z-20 opacity-50'></div>
 				<div className='absolute bottom-[-200px] left-[-10%] bg-gradient-radial from-sky-600 to-transparent w-96 h-96 rounded-full blur-2xl -z-20 opacity-50'></div>
 				<HeaderCard />
