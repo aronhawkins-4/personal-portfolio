696b031975a4ee429bd36d2846ca7c569ec43df4
Aron Hawkins <aron@sidekick.agency>
2023-08-30 16:55:19 -0500

    change website card text alignment on mobile and footer text alignment

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
 	return (
 		<div className='w-full p-8 mt-auto flex justify-center border-t border-t-gray-600 bg-gradient-to-tr from-black to-gray-400/25'>
-			<p className='text-white/75 font-light'>This site was made by Aron Hawkins using Next.js, TypeScript, and TailwindCSS. Deployed on Vercel.</p>
+			<p className='text-white/75 font-light text-center'>This site was made by Aron Hawkins using Next.js, TypeScript, and TailwindCSS. Deployed on Vercel.</p>
 		</div>
 	);
 };
